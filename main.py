a09f83898fb58fec246cc28c149b25cca29d73c2
louysotopo <louysotopo123@gmail.com>
2021-08-04 12:15:45 -0500

    jkjkj

--- main.py
+++ main.py
@@ -2,11 +2,3 @@
 from flask_cors import CORS
 from app import app
 from app.routes import api
-#Configuration 
-CORS(app,resources={r"/*": {"origins": "*"}})
-app.config['CORS_HEADERS'] = 'Content-Type'
-
-
-
-
-
